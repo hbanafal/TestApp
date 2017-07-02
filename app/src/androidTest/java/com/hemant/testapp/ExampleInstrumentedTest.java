eeef9d9275285e9a1bd0d27f628fa09a2e6bb526
root <root@ip-172-31-14-244.ap-south-1.compute.internal>
2017-07-02 16:31:56 +0000

    first commit on branch dev1

--- app/src/androidTest/java/com/hemant/testapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hemant/testapp/ExampleInstrumentedTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
+ * This is test1 
  * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
